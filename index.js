38f5af991db2ea474bdf16da53865362f316888c
Cyberbot143 <krishnasai952@gmail.com>
2017-10-01 02:53:12 -0400

    Registration and validations done

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
+/* ===================
+   Import Node Modules
+=================== */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const router = express.Router(); // Creates a new router object.
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+const config = require('./config/database'); // Mongoose Config
+const path = require('path'); // NodeJS Package for file paths
+const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const config = require('./config/database');
-const path = require('path'); // NodeJS Package for file 
-
-//Database Connection
+// Database Connection
 mongoose.Promise = global.Promise;
-mongoose.connect(config.uri,(err)=>{
-    if(err){
-        console.log('Could not connect to database',err);
-    }
-    else{
-        console.log(config.secret);
-        console.log('Connected to database :',config.db);
-    }
-}); 
-
-// Provide static directory for frontend
-app.use(express.static(__dirname + '/client/dist/'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
-  });
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('Could NOT connect to database: ', err);
+  } else {
+    console.log('Connected to database: ' + config.db);
+  }
 });
 
-app.listen(8080,() =>{
-    console.log("listening on port 8080");
+// Middleware
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use('/authentication', authentication);
+
+// Connect server to Angular 2 Index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+// Start Server: Listen on port 8080
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
 });
